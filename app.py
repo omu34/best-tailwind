2b7a481c80573ddcdf7c54b20e735af7c3fcd8dd
Bernard Omuse <79845295+omu34@users.noreply.github.com>
2021-10-13 15:16:55 +0300

    my tailwind

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask,render_template
  
 app=Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home_page(): 
-    return render_template('home.html')
+    return "<h1>mines</h1>"
+# render_template('base.html')
 
 if __name__ == '__main__':
     app.run(port=3400,debug=True)
